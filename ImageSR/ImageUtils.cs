fb0c428c97ca55a1a92ea8171a13e08bd83139c9
BAKAOLC <2565128043@qq.com>
2021-10-12 18:01:31 +0800

    init

---
+++ ImageSR/ImageUtils.cs
@@ -0,0 +1,258 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using RgbaImage = SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32>;
+
+namespace ImageSR
+{
+    public static partial class ImageUtils
+    {
+        struct FrameData
+        {
+            public RgbaImage[] Frames;
+            public int[] Delays;
+            public int TotalLength => Delays.Sum(x => x);
+            public bool IsGif;
+
+            public FrameData(RgbaImage image)
+            {
+                IsGif = image.Frames.Count > 1;
+                if (IsGif)
+                {
+                    Delays = new int[image.Frames.Count];
+                    Frames = new RgbaImage[image.Frames.Count];
+                    for (int i = 0; i < image.Frames.Count; i++)
+                    {
+                        Delays[i] = image.Frames[i].GetFrameDelay();
+                        Frames[i] = image.Frames.CloneFrame(i);
+                    }
+                }
+                else
+                {
+                    Delays = Array.Empty<int>();
+                    Frames = new RgbaImage[] { image };
+                }
+            }
+
+            public RgbaImage GetFrameOnTime(int t)
+            {
+                if (IsGif)
+                {
+                    int T = 0;
+                    for (int i = 0; i < Delays.Length; i++)
+                    {
+                        T += Delays[i];
+                        if (t < T)
+                            return Frames[i];
+                    }
+                    return Frames.Last();
+                }
+                else
+                    return Frames.First();
+            }
+        }
+
+        static int GetFrameDelay(this ImageFrame frame)
+            => frame.Metadata.GetGifMetadata().FrameDelay;
+
+        static int[] CalcFrames(FrameData[] datas)
+        {
+            var list = new List<int>();
+            list.Add(0);
+            foreach (var data in datas)
+            {
+                if (data.IsGif)
+                {
+                    int t = 0;
+                    foreach (var delay in data.Delays)
+                    {
+                        t += delay;
+                        if (!list.Contains(t))
+                            list.Add(t);
+                    }
+                }
+            }
+            return list.OrderBy(x => x).ToArray();
+        }
+
+        public static RgbaImage Synthesis(List<List<RgbaImage>> images)
+        {
+            int width = 0, height = 0;
+            var datas = new List<FrameData>();
+            var _datas = new List<List<FrameData>>();
+            var heights = new List<int>();
+            foreach (var row in images)
+            {
+                int w = 0, h = 0;
+                var list = new List<FrameData>();
+                foreach (var img in row)
+                {
+                    w += img.Width;
+                    h = Math.Max(h, img.Height);
+                    var data = new FrameData(img);
+                    datas.Add(data);
+                    list.Add(data);
+                }
+                _datas.Add(list);
+                width = Math.Max(width, w);
+                heights.Add(h);
+                height += h;
+            }
+            var frames = CalcFrames(datas.ToArray());
+            if (frames.Length > 1)
+            {
+                var resultImage = new RgbaImage(width, height);
+                resultImage.Metadata.GetGifMetadata().RepeatCount = 0;
+                for (int i = 0; i < frames.Length - 1; i++)
+                {
+                    int T = frames[i];
+                    int nextT = frames[i + 1];
+                    int delay = nextT - T;
+                    var frame = DrawFrame(width, height, _datas, heights, T);
+                    frame.Frames.RootFrame.Metadata.GetGifMetadata().FrameDelay = delay;
+                    resultImage.Frames.AddFrame(frame.Frames.RootFrame);
+                }
+                resultImage.Frames.RemoveFrame(0);
+                return resultImage;
+            }
+            else
+            {
+                return DrawFrame(width, height, _datas, heights, 0);
+            }
+        }
+
+        static void DrawImage(RgbaImage texture, RgbaImage image, int x, int y)
+        {
+            for (int py = 0; py < image.Height; py++)
+            {
+                for (int px = 0; px < image.Width; px++)
+                {
+                    int _x = x + px;
+                    int _y = y + py;
+                    if (_x >= 0 && _x < texture.Width && _y >= 0 && _y < texture.Height)
+                    {
+                        texture[_x, _y] = image[px, py];
+                    }
+                }
+            }
+        }
+
+        static readonly Rgba32 BackgroundColor = new Rgba32(1f, 1f, 1f, 1f);
+        static RgbaImage DrawFrame(int width, int height, List<List<FrameData>> datas, List<int> heights, int time)
+        {
+            var resultImage = new RgbaImage(width, height);
+            for (var py = 0; py < height; py++)
+                for (var px = 0; px < width; px++)
+                    resultImage[px, py] = BackgroundColor;
+            int y = 0;
+            for (var i = 0; i < datas.Count; i++)
+            {
+                int x = 0;
+                var row = datas[i];
+                var h = heights[i];
+                for (var j = 0; j < row.Count; j++)
+                {
+                    var img = row[j].GetFrameOnTime(time);
+                    DrawImage(resultImage, img, x, y + h - img.Height);
+                    x += img.Width;
+                }
+                y += h;
+            }
+            return resultImage;
+        }
+
+        public static RgbaImage[] Split(RgbaImage image, int row, int column)
+        {
+            int pw = image.Width / row;
+            int ph = image.Height / column;
+            int y = 0;
+            var list = new RgbaImage[row * column];
+            var id = 0;
+            for (int h = 0; h < row; h++)
+            {
+                int x = 0;
+                if (h == row - 1)
+                {
+                    int _h = image.Height - y;
+                    for (int w = 0; w < column; w++)
+                    {
+                        if (w == column - 1)
+                        {
+                            list[id] = GetSubImage(image, x, y, image.Width - x, _h);
+                        }
+                        else
+                        {
+                            list[id] = GetSubImage(image, x, y, pw, _h);
+                            x += pw;
+                        }
+                        id++;
+                    }
+                }
+                else
+                {
+                    for (int w = 0; w < column; w++)
+                    {
+                        if (w == column - 1)
+                        {
+                            list[id] = GetSubImage(image, x, y, image.Width - x, ph);
+                        }
+                        else
+                        {
+                            list[id] = GetSubImage(image, x, y, pw, ph);
+                            x += pw;
+                        }
+                        id++;
+                    }
+                    y += ph;
+                }
+            }
+            return list;
+        }
+
+        static RgbaImage GetSubImage(RgbaImage image, int x, int y, int w, int h)
+        {
+            var result = new RgbaImage(w, h);
+            if (image.Frames.Count > 1)
+            {
+                result.Metadata.GetGifMetadata().RepeatCount = 0;
+                for (int i = 0; i < image.Frames.Count; i++)
+                {
+                    var frame = GetSubImage(image.Frames.CloneFrame(i), x, y, w, h);
+                    frame.Frames.RootFrame.Metadata.GetGifMetadata().FrameDelay = image.Frames[i].Metadata.GetGifMetadata().FrameDelay;
+                    result.Frames.AddFrame(frame.Frames.RootFrame);
+                }
+                result.Frames.RemoveFrame(0);
+            }
+            else
+            {
+                for (int py = 0; py < h; py++)
+                {
+                    for (int px = 0; px < w; px++)
+                    {
+                        int _x = x + px;
+                        int _y = y + py;
+                        if (_x >= 0 && _x < image.Width && _y >= 0 && _y < image.Height)
+                        {
+                            result[px, py] = image[_x, _y];
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static void SaveImage(RgbaImage image, string name)
+        {
+            if (image.Frames.Count > 1)
+            {
+                image.SaveAsGif(name);
+            }
+            else
+            {
+                image.SaveAsPng(name);
+            }
+        }
+    }
+}
---
+++ ImageSR/Program.cs
@@ -0,0 +1,212 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ImageSR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (ChooseWorkType()) ;
+        }
+
+        static bool ChooseWorkType()
+        {
+            Console.Clear();
+            Console.WriteLine("请选择要执行的操作：");
+            Console.WriteLine("1. 合成图像");
+            Console.WriteLine("2. 分割图像");
+            Console.WriteLine("3. 退出");
+            string choose;
+            string[] chooseList = { "1", "2", "3" };
+            while (!chooseList.Contains(choose = Console.ReadLine().Trim())) ;
+            switch (choose)
+            {
+                case "1":
+                    Synthesis();
+                    break;
+                case "2":
+                    Split();
+                    break;
+                case "3":
+                    return false;
+            }
+            return true;
+        }
+
+        static void Synthesis()
+        {
+            Console.Clear();
+            string input;
+            var row = 0;
+            var columns = new List<int>();
+            var sources = new List<List<string>>();
+            while (true)
+            {
+                Console.WriteLine("请输入要合成的图像行数：");
+                input = Console.ReadLine().Trim();
+                if (int.TryParse(input, out row))
+                {
+                    if (row > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("合成行数不可小于 1");
+                    }
+                }
+                else if (!string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("数值格式错误");
+                }
+            }
+            for (int i = 0; i < row; i++)
+            {
+                while (true)
+                {
+                    Console.WriteLine($"请输入第 {i + 1} 行的图像数量：");
+                    input = Console.ReadLine().Trim();
+                    if (int.TryParse(input, out int count))
+                    {
+                        if (count > 0)
+                        {
+                            columns.Add(count);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("合成图像数不可小于 1");
+                        }
+                    }
+                    else if (!string.IsNullOrWhiteSpace(input))
+                    {
+                        Console.WriteLine("数值格式错误");
+                    }
+                }
+            }
+            for (int i = 0; i < row; i++)
+            {
+                Console.WriteLine($"开始设置第 {i + 1} 行的图像");
+                var files = new List<string>();
+                for (int j = 0; j < columns[i]; j++)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine($"请输入第 {j + 1} 张图的图像路径：");
+                        input = Console.ReadLine().Trim();
+                        if (File.Exists(input))
+                        {
+                            files.Add(input);
+                            break;
+                        }
+                        else if (!string.IsNullOrWhiteSpace(input))
+                            Console.WriteLine("文件不存在");
+                    }
+                }
+                sources.Add(files);
+            }
+            Console.WriteLine("生成中……");
+            var images = sources.Select(x => x.Select(y => Image.Load<Rgba32>(y)).ToList()).ToList();
+            var result = ImageUtils.Synthesis(images);
+            string filename = "output" + (result.Frames.Count > 1 ? ".gif" : ".png");
+            ImageUtils.SaveImage(result, filename);
+            Console.WriteLine("生成完毕，文件已储存至：" + filename);
+            Console.WriteLine("按任意键继续");
+            Console.ReadKey();
+        }
+
+        static void Split()
+        {
+            Console.Clear();
+            string input;
+            string file = string.Empty;
+            int row = 0, column = 0;
+            while (true)
+            {
+                Console.WriteLine("请输入要分隔的图像文件位置：");
+                input = Console.ReadLine().Trim();
+                if (File.Exists(input))
+                {
+                    file = input;
+                    break;
+                }
+                else if (!string.IsNullOrWhiteSpace(input))
+                    Console.WriteLine("文件不存在");
+            }
+            while (true)
+            {
+                Console.WriteLine("请输入要分割的行数：");
+                input = Console.ReadLine().Trim();
+                if (int.TryParse(input, out row))
+                {
+                    if (row > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("分割行数不可小于 1");
+                    }
+                }
+                else if (!string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("数值格式错误");
+                }
+            }
+            while (true)
+            {
+                Console.WriteLine("请输入要分割的列数：");
+                input = Console.ReadLine().Trim();
+                if (int.TryParse(input, out column))
+                {
+                    if (column > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("分割列数不可小于 1");
+                    }
+                }
+                else if (!string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("数值格式错误");
+                }
+            }
+            if (!Directory.Exists("Output"))
+            {
+                Directory.CreateDirectory("Output");
+            }
+            Console.WriteLine("生成中……");
+            var name = Path.GetFileNameWithoutExtension(file);
+            var source = Image.Load<Rgba32>(file);
+            var result = ImageUtils.Split(source, row, column);
+            var saves = new List<string>();
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i].Frames.Count > 1)
+                {
+                    var filename = $"Output/{name}_{i + 1}.gif";
+                    result[i].SaveAsGif(filename);
+                    saves.Add(filename);
+                }
+                else
+                {
+                    var filename = $"Output/{name}_{i + 1}.png";
+                    result[i].SaveAsPng(filename);
+                    saves.Add(filename);
+                }
+            }
+            Console.WriteLine("生成完毕，文件已储存至：");
+            foreach (var path in saves)
+                Console.WriteLine(path);
+            Console.WriteLine("按任意键继续");
+            Console.ReadKey();
+        }
+    }
+}
